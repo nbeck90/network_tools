98971fca86040fc7a1d0c6bee0b82ff623f5ffc0
Nick Becker <nbeck90@uw.edu>
2015-02-15 15:50:40 -0800

    Add tests for HTTP1

--- test_client.py
+++ test_client.py
@@ -1,5 +1,4 @@
 from client import client
-import pytest
 
 
 def test_response_ok():
@@ -8,5 +7,32 @@ def test_response_ok():
     con_type = "Content-Type: text/plain\r\n"
     body = "Content length: {}".format(21)
     # Length of message from file name to end of line
+    result = "{}{}{}".format(result, con_type, body)
+    assert client(msg) == result
+
+
+def test_response_post():
+    msg = "POST /path/to/myindex.html HTTP/1.1\r\nHost: localhost:50000\r\n"
+    result = "HTTP/1.1 405 ERROR\r\n"
+    con_type = "Content-Type: text/plain\r\n"
+    body = "ERROR 405, POST METHOD NOT ALLOWED\r\n"
+    result = "{}{}{}".format(result, con_type, body)
+    assert client(msg) == result
+
+
+def test_response_not_supported():
+    msg = "GET /path/to/myindex.html HTTP/1.0\r\nHost: localhost:50000\r\n"
+    result = "HTTP/1.1 505 ERROR\r\n"
+    con_type = "Content-Type: text/plain\r\n"
+    body = "ERROR 505, HTTP/1.0 NOT SUPPORTED\r\n"
+    result = "{}{}{}".format(result, con_type, body)
+    assert client(msg) == result
+
+
+def test_bad_response():
+    msg = "This"
+    result = "HTTP/1.1 400 ERROR\r\n"
+    con_type = "Content-Type: text/plain\r\n"
+    body = "ERROR 400, BAD REQUEST\r\n"
     result = "{}{}{}".format(result, con_type, body)
     assert client(msg) == result
